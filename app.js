f175c02b53d4d06e87a5bf037aa6f2c6d1f29c20
Carole SOTIRIO <ca.sotirio@gmail.com>
2020-11-13 20:37:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
